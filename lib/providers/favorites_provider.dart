9b25856289a9a264d682ac789a5e00c6955ad4de
duc77rma <duc7rma@gmail.com>
2024-05-16 01:12:00 +0700

    build app

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavoritesMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoritesMealsNotifier() : super([]);
+
+  bool toggleMealFavoritesStatus(Meal meal) {
+    final mealsIsFavorite = state.contains(meal);
+
+    if (mealsIsFavorite) {
+      state = state.where((ele) => ele.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favoritesMealsProvider =
+    StateNotifierProvider<FavoritesMealsNotifier, List<Meal>>((ref) {
+  return FavoritesMealsNotifier();
+});
---
+++ lib/screens/categories.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen(
+      {super.key, required this.availableMeal});
+
+  final List<Meal> availableMeal;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeal
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+              meals: filteredMeals,
+              title: category.title,
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
+      ],
+    );
+  }
+}
